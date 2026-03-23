babd26e2cfb52138d66e16bab6ed9109f216fe8f
Necronicle <necronicle@gmail.com>
2026-03-24 00:01:10 +0300

    Prepare v0.1.3 release

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "bypass.whitelist"
         minSdk = 23
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
